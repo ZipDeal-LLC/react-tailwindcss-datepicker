63adc35ea497305f0a5bbb98254849231c5f323d
andriialbatov <andrii.albatov.ddi@gmail.com>
2022-12-13 10:28:39 -0500

    dist

---
+++ dist/components/Datepicker.d.ts
@@ -0,0 +1,37 @@
+import React from "react";
+interface Props {
+    primaryColor?: string;
+    value: {
+        startDate: string | Date | null;
+        endDate: string | Date | null;
+    } | null;
+    onChange: (value: {
+        startDate: string | Date | null;
+        endDate: string | Date | null;
+    } | null) => void;
+    useRange?: boolean;
+    showFooter?: boolean;
+    showShortcuts?: boolean;
+    configs?: {
+        shortcuts?: {
+            today?: string;
+            yesterday?: string;
+            past?: (period: number) => string;
+            currentMonth?: string;
+            pastMonth?: string;
+        } | null;
+        footer?: {
+            cancel?: string;
+            apply?: string;
+        } | null;
+        past?: boolean;
+    } | null;
+    asSingle?: boolean;
+    placeholder?: string;
+    separator?: string;
+    i18n?: string;
+    dateValueFormat?: string;
+    dateStringFormat?: string;
+}
+declare const Datepicker: React.FC<Props>;
+export default Datepicker;
---
+++ dist/types/index.d.ts
@@ -0,0 +1,28 @@
+export interface Period {
+    start: string | null;
+    end: string | null;
+    marker?: string;
+}
+export interface Configs {
+    shortcuts?: {
+        today?: string;
+        yesterday?: string;
+        past?: (period: number) => string;
+        currentMonth?: string;
+        pastMonth?: string;
+    } | null;
+    footer?: {
+        cancel?: string;
+        apply?: string;
+    } | null;
+    past?: boolean;
+}
+export interface ShortcutsItem {
+    text?: string;
+    daysNumber?: number;
+    period?: {
+        start: string;
+        end: string;
+        marker?: string;
+    };
+}
